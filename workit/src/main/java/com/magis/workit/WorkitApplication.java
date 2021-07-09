25623e1c1561aa3bd4f920bba2203c08487130a2
유정연 <yujy0302@gmail.com>
2021-07-09 22:38:46 +0900

    초기설정 완료

---
+++ workit/src/main/java/com/magis/workit/WorkitApplication.java
@@ -0,0 +1,13 @@
+package com.magis.workit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkitApplication.class, args);
+	}
+
+}
---
+++ workit/src/test/java/com/magis/workit/WorkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magis.workit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
